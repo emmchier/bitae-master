43511f3d9b8383eb4770b85a3889571980c43ce5
Emmanuel Chierchie <emmchierchie@gmail.com>
2021-07-31 13:43:57 -0300

    navbar general desk ready

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-      if($(this).scrollTop() > 700){
-        $('#scroll').fadeIn();
-      } else {
-        $('#scroll').fadeOut();
-      }
-    });
-    $('#scroll').click(function(){
-      $("html, body").animate({ scrollTop: 0 }, 500);
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 700) {
+      $("#scroll").fadeIn();
+      $(".navbar").addClass("changeState");
+    } else {
+      $("#scroll").fadeOut();
+      $(".navbar").removeClass("changeState");
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
+  });
 });
